7e221700eb47977828881a5f3de4bbbf6f7f4f18
sokrat <sergey.sipatov@gmail.com>
2015-03-27 19:41:36 +0300

    universalization

--- TreeViewBehavior.php
+++ TreeViewBehavior.php
@@ -9,30 +9,83 @@
 namespace consultnn\treeview;
 
 use yii\base\Behavior;
+use yii\db\ActiveRecord;
 
+/**
+ * Class TreeViewBehavior
+ * @property \yii\db\ActiveRecord owner
+ * @package consultnn\treeview
+ */
 class TreeViewBehavior extends Behavior
 {
+    public $parentAttribute = 'parent_id';
+    public $positionAttribute = 'position';
+
+    public $events = [
+        ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete',
+        ActiveRecord::EVENT_BEFORE_INSERT => 'setPosition',
+        ActiveRecord::EVENT_BEFORE_UPDATE => 'setPosition',
+    ];
+
+    public function events()
+    {
+        return $this->events;
+    }
+
     /**
-     * @param string|null $parentId
+     * @inheritdoc
+     */
+    public function beforeDelete()
+    {
+        /** @var ActiveRecord[] $children */
+        $children = $this->owner->findAll([$this->parentAttribute => $this->owner->getPrimaryKey()]);
+        if (!empty($children)) {
+            foreach ($children as $child) {
+                $child->delete();
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasChildren()
+    {
+        return $this->owner->findOne([$this->parentAttribute => $this->owner->getPrimaryKey()]) != null;
+    }
+
+    /**
+     * @return null|ActiveRecord[]
+     */
+    public function getChildren()
+    {
+        return $this->owner
+            ->find()
+            ->where([$this->parentAttribute => $this->owner->getPrimaryKey()])
+            ->orderBy('position')
+            ->all();
+    }
+
+    /**
+     * @param string|null $parent
      * @param int $position
      * @return bool
      */
-    public function move($parentId = null, $position = null)
+    public function move($parent = null, $position = null)
     {
-        if ($parentId) {
-            $parentId = new \MongoId($parentId);
-        } else {
-            $parentId = null;
-        }
-
-        if ($parentId == $this->owner->id) {
+        if ($parent == $this->owner->getPrimaryKey()) {
             return false;
         }
 
-        $this->owner->parent_id = $parentId;
+        $this->owner->{$this->parentAttribute} = $parent;
         $this->calculatePosition($position);
 
         return $this->owner->save();
+    }
+
+    public function setPosition($event)
+    {
+        $this->calculatePosition();
     }
 
     /**
@@ -41,39 +94,39 @@ class TreeViewBehavior extends Behavior
      */
     public function calculatePosition($position = null)
     {
-        if (!empty($this->owner->position) && $position === null) {
-            return $this->owner->position;
+        if (!empty($this->owner->{$this->positionAttribute}) && $position === null) {
+            return $this->owner->{$this->positionAttribute};
         } elseif (empty($position)) {
             return $this->setMinPosition();
         }
 
-        $condition['parent_id'] = $this->owner->parent_id;
-        if ($this->owner->id) {
-            $condition['_id'] = ['$ne' => $this->owner->_id];
+        $query = $this->owner->find();
+        $query->andWhere([$this->parentAttribute => $this->owner->{$this->parentAttribute}]);
+        if ($this->owner->getPrimaryKey()) {
+            $query->andWhere(['NOT IN', $this->owner->primaryKey(), $this->owner->getPrimaryKey()]);
         }
 
-        $owner = $this->owner;
-        $brothers = $owner::find()->where($condition)->orderBy('position')->offset($position - 1)->limit(2)->all();
+        $brothers =$query->orderBy('position')->offset($position - 1)->limit(2)->all();
         switch (count($brothers)) {
             case 0:
                 $this->setMinPosition();
                 break;
             case 1:
-                $this->owner->position = ceil($brothers[0]->position + 1);
+                $this->owner->{$this->positionAttribute} = ceil($brothers[0]->{$this->positionAttribute} + 1);
                 break;
             case 2:
                 list($prev, $next) = $brothers;
-                $rise = ($next->position - $prev->position) / 2;
+                $rise = ($next->{$this->positionAttribute} - $prev->{$this->positionAttribute}) / 2;
                 if ($rise == 0) {
-                    self::updatePositions($this->owner->parent_id);
-                    $this->calculatePosition($position);
+                    $this->updatePositions();
+                    $this->calculatePosition(null, $position);
                 } else {
-                    $this->owner->position = $prev->position + $rise;
+                    $this->owner->{$this->positionAttribute} = $prev->{$this->positionAttribute} + $rise;
                 }
                 break;
         }
 
-        return $this->owner->position;
+        return $this->owner->{$this->positionAttribute};
     }
 
     /**
@@ -81,37 +134,33 @@ class TreeViewBehavior extends Behavior
      */
     private function setMinPosition()
     {
-        $owner = $this->owner;
-        $min = $owner::find()->where(['parent_id' => $this->owner->parent_id])->min('position');
+        $min = $this->owner->find()->where([$this->parentAttribute => $this->owner->getPrimaryKey()])->min('position');
         $minPosition = $min / 2;
 
         if (!$min) {
-            $this->owner->position = 1;
+            $this->owner->{$this->positionAttribute} = 1;
         } elseif ($minPosition != 0) {
-            $this->owner->position = $minPosition;
+            $this->owner->{$this->positionAttribute} = $minPosition;
         } else {
-            $this->updatePositions($this->owner->parent_id);
+            $this->updatePositions();
             $this->setMinPosition();
         }
 
-        return $this->owner->position;
+        return $this->owner->{$this->positionAttribute};
     }
 
-    /**
-     * @param $parentId
-     */
-    private function updatePositions($parentId)
+    private function updatePositions()
     {
-        if (!is_null($parentId)) {
-            $parentId = new \MongoId($parentId);
-        }
-
         $position = 0;
 
-        /** @var self[] $rubrics */
-        $rubrics = $this->owner->find()->where(['parent_id' => $parentId])->orderBy('position')->all();
-        foreach ($rubrics as $rubric) {
-            $rubric->updateAttributes(['position' => ++$position]);
+        /** @var ActiveRecord[] $objects */
+        $objects = $this->owner->find()->where([$this->parentAttribute => $this->owner->getPrimaryKey()])->orderBy('position')->all();
+        foreach ($objects as $object) {
+            $object->updateAttributes(['position' => ++$position]);
         }
     }
+
+
+
+
 }
